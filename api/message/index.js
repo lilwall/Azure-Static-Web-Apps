4e78e08e05f1fbc955b46887ed79e0131df5378e
A.J <amurillo20@gmail.com>
2021-07-23 22:27:06 +0000

    API commit reponse

--- api/message/index.js
+++ api/message/index.js
@@ -1,7 +1,10 @@
 module.exports = async function (context, req) {
-    const fetch= require ('node-fetch');
-
-    await fetch ('https://rick-and-morty-api-phrases.herokuapp.com/phrases/en_us/random').then(response => response.json()).then(data => response.data);
+    const fetch= require('node-fetch');
+    
+    
+    
+    let response;
+    await fetch('https://rick-and-morty-api-phrases.herokuapp.com/phrases/en_us/random').then(response => response.json()).then(data => response=data).catch(console.log("error"));
     context.res.json({
     text: String(response.phrase)
     });
