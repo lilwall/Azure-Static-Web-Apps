782132a6b9bc99adf96ae5ef6ee838fe8492b12b
A.J <amurillo20@gmail.com>
2021-07-23 22:13:17 +0000

    API for training random message

--- api/message/index.js
+++ api/message/index.js
@@ -1,7 +1,7 @@
 module.exports = async function (context, req) {
     const fetch= require ('node-fetch');
 
-    await fetch ('https://rick-and-morty-api-phrases.herokuapp.com/').then(response => response.json()).then(data => response.data);x
+    await fetch ('https://rick-and-morty-api-phrases.herokuapp.com/phrases/en_us/random').then(response => response.json()).then(data => response.data);
     context.res.json({
     text: String(response.phrase)
     });
